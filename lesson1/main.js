98544d36b9d4f8d93c5feee4dfcda555b24008ee
Esk4nor <Bloodtera61@gmail.com>
2023-05-02 15:23:23 +0300

    h.w.1 executed 1

---
+++ lesson1/main.js
@@ -0,0 +1,52 @@
+// h.w.1  //
+
+let hello = `hello`
+let owu = 'owu'
+let com = 'com'
+let ua = 'ua'
+let number1 = 1
+let nubmer2 = 10
+let number3 = -999
+let  number4 = 123
+let number5 = 3.14
+let number6=2.7
+let number7=16
+let booleant=true
+let booleanf= false
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(number1);
+console.log(nubmer2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number6);
+console.log(number7);
+console.log(booleant);
+console.log(booleanf);
+
+
+// h.w.2
+
+let firstName = 'Denys'
+let middleName = 'Bogdanovich'
+let lastName = 'Lagutkin'
+let person = `${lastName} ${firstName} ${middleName}`
+console.log(person);
+
+// h.w.3 //
+
+let a= 100
+let b='100'
+let c=true
+console.log(typeof a, typeof b,typeof c);
+
+// h.w.4 //
+
+let name = prompt('enter your name:')
+let mname=prompt('enter your middlename')
+let lname=prompt('enter your lastname')
+let person2= `${lastName} ${name} ${mname}`
+console.log(person2)
